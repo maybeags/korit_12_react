6017d79b25e331251d7d3d47f58d22061d18e992
maybeags <suren0709@gmail.com>
2026-03-04 09:50:57 +0900

    feat : 1st lesson

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,11 +1,14 @@
 import Hello from './Hello.jsx';
 import Drink from './Drink.jsx';
 import MyComponent from './MyComponent.jsx';
+import MyComponent2 from './MyComponent2.jsx';
 
 export default function App() {
 
   return(
     <>
+      <MyComponent2 isLoggedin={true} />
+      <MyComponent2 isLoggedin={false} />
       <MyComponent />
       <Drink drink='coffee' />
       <Hello firstName='Jone' lastName='Doe'/>
--- myapp/src/Drink.jsx
+++ myapp/src/Drink.jsx
@@ -1,4 +1,13 @@
+import HeaderText from "./HeaderText"
+
 export default function Drink({drink}) {
 
-  return <h1>Would you like to drink some {drink} ?</h1>
+  return (
+    <>
+      <h1>
+        Would you like to drink some {drink} ?
+        <HeaderText text='추가텍스트' />
+      </h1>
+    </>
+  )
 }
