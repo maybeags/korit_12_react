748559b680109844301ad2f905e5a338c6e78510
maybeags <suren0709@gmail.com>
2026-03-09 15:38:44 +0900

    feat: openweather api usage

--- weatherapp/src/App.tsx
+++ weatherapp/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
@@ -8,11 +8,30 @@ function App() {
     icon: '',
   });
 
-  return (
-    <>
-      
+  useEffect(()=>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Busan&units=Metric&APIkey=여러분_API_KEY`)
+    .then(response => response.json())
+    .then(data => {
+      setWeather({
+        temp: data.main.temp,
+        desc: data.weather[0].description,
+        icon: data.weather[0].icon
+      })
+    })
+    .catch(err => console.log(err));
+  },[]);
+
+
+    return (
+      <>
+        <p>기온 : {weather.temp} ℃</p>
+        <p>설명 : {weather['desc']}</p>
+        <p>아이콘 string : {weather.icon}</p>
+        <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="날씨아이콘" />
     </>
-  )
+    )
+
+  
 }
 
 export default App
