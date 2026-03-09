f85e7e450cbac8ab3de4be6a163ea8f06195e760
maybeags <suren0709@gmail.com>
2026-03-09 14:26:04 +0900

    feat: endpoint

---
+++ weatherapp/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [ weather, setWeather ] = useState({
+    temp: '',
+    desc: '',
+    icon: '',
+  });
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
