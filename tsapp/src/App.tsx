a32c8bc0548697678be7af350ae7db5254724712
maybeags <suren0709@gmail.com>
2026-03-09 09:51:06 +0900

    feat: ts lesson props

---
+++ tsapp/src/App.tsx
@@ -0,0 +1,13 @@
+import HelloComponent from './HelloComponent'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <HelloComponent name='김영' age='twenty' />
+    </>
+  )
+}
+
+export default App
